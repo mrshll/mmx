20ebe663440502294d31cb53e823eff634fcf99f
mrshll <mmoutenot@gmail.com>
2021-07-18 22:07:57 -0400

    +

--- src/energy.sh
+++
@@ -1,4 +0,0 @@
-!#/bin/bash
-
-curl https://monitoringapi.solaredge.com/site/1368783/energyDetails?api_key=$SOLAR_EDGE_API_KEY&meters=PRODUCTION,CONSUMPTION&timeUnit=DAY&startTime=2021-04-19%2000:00:00&endTime=2021-07-18%2000:00:00 > ../data/energy.json
-js
